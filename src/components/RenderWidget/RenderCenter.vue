13c2bf98bef779e83530731e017a594ed7712ce4
optimistic-l1 <87272754+optimistic-l1@users.noreply.github.com>
2022-08-11 22:13:42 +0800

    Add files via upload

---
+++ src/components/RenderWidget/RenderCenter.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="render-widget" :class="{'active': curCompId === itemData.id}" @click="compClick">
+    <component :is="itemData.component" v-bind="itemData">
+    <slot></slot>
+    </component>
+  
+  </div>
+</template>
+
+<script>
+import { getComponents } from '@/utils/register'
+const compListObj = getComponents(require.context('@/components/ControlConfigs', true, /.vue/))
+export default {
+  components: {
+    ...compListObj
+  },
+  props: {
+    curCompId: {
+      type: Number,
+      default: ''
+    },
+    itemData: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  methods: {
+    compClick() {
+      if(this.curCompId === this.itemData.id) {
+        return
+      }
+      this.$emit('chooseComp', this.itemData)
+    }
+  }
+  
+}
+</script>
+<style>
+  .box {
+    width: 100%;
+    height: 100%;
+    background: #eee;
+    box-sizing: border-box;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .left {
+    width: 25%;
+  }
+  .center {
+    width: 46%;
+  }
+  .right {
+    width: 25%;
+  }
+  .left,.center,.right {
+    background: #fff;
+    height: calc(100vh - 40px);
+    border-radius: 10px;
+  }
+  h4 {
+    padding-left: 10px;
+  }
+  .render-widget.active {
+    border: 1px solid blue;
+    padding: 3px;
+  }
+</style>
---
+++ src/components/RenderWidget/RenderRight.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-form ref="form" label-width="80px">
+    <template v-for="(obj, key, index)  in field">
+      <component
+        :is="'attr-' + obj.type"
+        v-bind="obj"
+        v-model="chontrol.curComponent[key]"
+        :key="index"></component>
+    </template>
+    <slot></slot>
+  </el-form>
+</template>
+
+<script>
+export default {
+  inject: ["chontrol"],
+  computed: {
+    field() {
+      return this.$fields[this.chontrol.curComponent.component]
+    }
+  }
+}
+</script>
+<style>
+  .el-form {
+    padding: 20px;
+  }
+</style>
