50738d7993b09bcbc8ed8786b90030922e41f5e7
Oreo-Cookie1 <110093234+Oreo-Cookie1@users.noreply.github.com>
2022-08-13 18:36:32 +0800

    颜色选择器加开关

---
+++ src/components/AttrForms/AttrSwitch/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <el-switch
+      v-model="value"
+      :inactive-text="label"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
+</div>
+</template>
+
+<script>
+import attrMixin from '@/mixins/mixin'
+  export default {
+    name: 'AttrSwitch',
+    mixins: [attrMixin]
+  }
+</script>
