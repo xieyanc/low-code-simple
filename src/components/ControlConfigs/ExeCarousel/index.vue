fe5866579ca2844504fcdef856c58ba2e2ba75c3
dianxi <1756303953@qq.com>
2022-08-14 14:46:51 +0800

    重新修复了轮播图图片问题，但是由于右边属性栏功能的缺失依然无法进行图片的插入动作

--- src/components/ControlConfigs/ExeCarousel/index.vue
+++ src/components/ControlConfigs/ExeCarousel/index.vue
@@ -11,8 +11,8 @@
         :loop="loop"
         :direction="direction"
     >
-      <el-carousel-item v-for="item in imageUrl" :key="item.id">
-        <img :src="item" />
+      <el-carousel-item v-for="item in imageURL" :key="item.id">
+        <img :src="item.url" />
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -21,6 +21,28 @@
 <script>
 export default {
   name: "ExeCarousel",
+  data(){
+    return{
+      imageURL: [
+        {
+          id: 0,
+          url: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ffile02.16sucai.com%2Fd%2Ffile%2F2014%2F0827%2Fc0c92bd51bb72e6d12d5b877dce338e8.jpg&refer=http%3A%2F%2Ffile02.16sucai.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1650366164&t=d27e21189b6673f5e8c641161ede768b"
+        },
+        {
+          id: 1,
+          url: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ffile02.16sucai.com%2Fd%2Ffile%2F2014%2F0827%2Fc0c92bd51bb72e6d12d5b877dce338e8.jpg&refer=http%3A%2F%2Ffile02.16sucai.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1650366164&t=d27e21189b6673f5e8c641161ede768b"
+        },
+        {
+          id: 2,
+          url: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ffile02.16sucai.com%2Fd%2Ffile%2F2014%2F0827%2Fc0c92bd51bb72e6d12d5b877dce338e8.jpg&refer=http%3A%2F%2Ffile02.16sucai.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1650366164&t=d27e21189b6673f5e8c641161ede768b"
+        },
+        {
+          id: 3,
+          url: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ffile02.16sucai.com%2Fd%2Ffile%2F2014%2F0827%2Fc0c92bd51bb72e6d12d5b877dce338e8.jpg&refer=http%3A%2F%2Ffile02.16sucai.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1650366164&t=d27e21189b6673f5e8c641161ede768b"
+        }
+      ]
+    }
+  },
   props: {
     imageUrl: {
       type: String,
