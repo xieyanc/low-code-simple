f57ec4c317b2b77db0d9136749ee0bb57da000bf
llllll1022leo <110092371+llllll1022leo@users.noreply.github.com>
2022-08-17 14:51:10 +0800

    Add files via upload
    一直想添加用户可以自定义字体及其颜色、大小，但是老是解决不了...

---
+++ src/components/ControlConfigs/ExeText/index.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div>
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-input :class="font" >
+      @import "./font.css"
+    </el-input>
+  </div>
+  
+    
+</template>
+
+<script>
+import LabelEdit from 'label-edit'
+export default {
+  name: "ExeText",
+
+  props: {
+    msg: String,
+    font: {
+      type: String,
+      default: "Helvetica Neue",
+    },
+    fontsize: {
+      type: Number,
+      default: 12,
+    },
+    fontcolor: {
+      type: Number,
+      default: "#000000"
+    }
+  },
+
+  data() {
+    return {
+      input: ''
+    }
+  }
+}
+
+</script>
+
+
