373a9d635aa17e8e8eae0fc1ac8a2ed59f6b75db
optimistic-l1 <87272754+optimistic-l1@users.noreply.github.com>
2022-08-24 20:57:42 +0800

    Add files via upload

---
+++ src/components/ControlConfigs/ExeHeadPortrait/index.vue
@@ -0,0 +1,38 @@
+
+<template>
+
+   <div class="demo-type">
+    <span>
+      <el-avatar :icon="icon"></el-avatar>
+    </span>
+    <span>
+      <el-avatar :src="imageicon"></el-avatar>
+    </span>
+    
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ExeHeadPortrait",
+
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    imageicon: {
+      type: String,
+      default: ''
+    },
+  },
+
+  data() {
+    return {
+     
+
+    };
+  },
+};
+</script>
