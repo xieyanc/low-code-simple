08d8d708de1c682c3d16c0ff0c18bdd87b494107
optimistic-l1 <87272754+optimistic-l1@users.noreply.github.com>
2022-08-24 20:58:11 +0800

    Add files via upload

---
+++ src/components/ControlConfigs/ExeUpload/index.vue
@@ -0,0 +1,40 @@
+
+<template>
+
+   <el-upload
+  class="upload-demo"
+  action="https://jsonplaceholder.typicode.com/posts/"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :file-list="fileList"
+  list-type="picture">
+  <el-button size="small" type="primary">点击上传</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+</el-upload>
+
+</template>
+
+<script>
+export default {
+  name: "ExeUpload",
+
+  props: {
+    
+  },
+
+  data() {
+    return {
+     
+fileList: [{name: 'food.jpeg', url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2Ff%2F538d42668170d.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1662346240&t=e5ac863188aa4446b2ea6fc9fc8466fa'}]
+    };
+  },
+  methods: {
+     handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      }
+  },
+};
+</script>
